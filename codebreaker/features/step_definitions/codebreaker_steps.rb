c86073231d4afa2e772e40ed1ed828c928454a10
Leighton Shank <leighton@getloaded.com>
2014-05-13 17:18:34 -0400

    first few chapters on codebreaker game

---
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,34 @@
+class Output
+  def messages
+    @messages ||= []
+  end
+
+  def puts(message)
+    messages << message
+  end
+end
+
+def output
+  @output ||= Output.new
+end
+
+Given /^I am not yet playing$/ do
+end
+
+When /^I start a new game$/ do
+  game = Codebreaker::Game.new(output)
+  game.start
+end
+
+Then /^I should see "([^"]*)"$/ do |message|
+  output.messages.should include(message)
+end
+
+#Given /^the secret code is "([^"]*)"$/ do |code|
+#end
+
+#When /^I guess "([^"]*)"$/ do |guess|
+#end
+
+#Then /^the mark should be "([^"]*)"$/ do |mark|
+#end
